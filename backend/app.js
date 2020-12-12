86cef33521ad1727683ee96da937cdd0e22a1b40
lucaslrf <lucasrodrigues.lrf@gmail.com>
2020-12-12 00:02:30 -0300

    struct backend

---
+++ backend/app.js
@@ -0,0 +1,4 @@
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const db = require("./db/db");
---
+++ backend/db/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+let mongo = mongoose.connect('mongodb://localhost/blog', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+}, (err) => {
+    if (!err) {
+        console.log('MongoDB Connection Succeeded.')
+    } else {
+        console.log('Error in DB connection: ' + err)
+    }
+});
